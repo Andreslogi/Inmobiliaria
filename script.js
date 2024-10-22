062d1f0399d66c59008f8d5941665aa90b93fc87
Andres Lopez <165798295+Andreslogi@users.noreply.github.com>
2024-10-22 09:44:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Función para mostrar las propiedades según el botón clicado (venta o arriendo)
+function mostrarPropiedades(tipo) {
+    const seccionVenta = document.getElementById('venta');
+    const seccionArriendo = document.getElementById('arriendo');
+
+    if (tipo === 'venta') {
+        seccionVenta.classList.remove('oculto');
+        seccionArriendo.classList.add('oculto');
+    } else if (tipo === 'arriendo') {
+        seccionVenta.classList.add('oculto');
+        seccionArriendo.classList.remove('oculto');
+    }
+}
